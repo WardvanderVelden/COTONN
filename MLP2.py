2742c49c9b640c05cade8908478167c952bda448
Ward van der Velden <wardtuur@gmail.com>
2018-05-05 13:08:52 +0200

    TensorFlow testing and MLP reworking

--- MLP2.py
+++ MLP2.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import NeuralNetworkManager
+import random
 
 # The MLP class as presented in this file is a bare bone object oriented approach to creating an MLP
 # for the purpose of capturing the behaviour of a static controller (table). 
@@ -78,8 +79,12 @@ class MLP:
             return self.network
         
         # TensorFlow MLP test with an extremely simple network with only 1 layer in between to get my head around tf
-        def tensorFlowTest(self):
-            self.s = tf.placeholder(tf.float32, shape=[None, self.layers[0]]) # placeholder tensors into which the batches will be loaded
+        def tensorFlowTest(self, nnm):
+            # instantiate an interactive tensorflow session 
+            sess = tf.InteractiveSession()
+            
+            # placeholder tensors into which the batches will be loaded
+            self.s = tf.placeholder(tf.float32, shape=[None, self.layers[0]])
             self.u = tf.placeholder(tf.float32, shape=[None, self.layers[-1]]) 
             
             # tensors which contain the weights and biases (weight matrices and bias vectors)
@@ -92,11 +97,96 @@ class MLP:
             self.weights[1]= tf.Variable(tf.random_normal([self.layers[1], self.layers[-1]]))
             self.biases[1] = tf.Variable(tf.random_normal([self.layers[-1]]))
             
-            intermediate = tf.add(tf.matmul(self.s, self.weights[0]), self.biases[0]) # first layer with tensor t0 = x*W0+b0
-            self.network = tf.add(tf.matmul(intermediate, self.weights[1]), self.biases[1]) #second layer with tensor y = t0*W1+b1
+            # initialize variables    
+            sess.run(tf.global_variables_initializer())
             
-            # self.loss_function = tf.reduce_mean(tf.sub(self.u - self.network))
+            # initialize network
+            self.intermediate = tf.add(tf.matmul(self.s, self.weights[0]), self.biases[0]) # first layer with tensor t0 = x*W0+b0
+            self.network = tf.add(tf.matmul(self.intermediate, self.weights[1]), self.biases[1]) #second layer with tensor y = t0*W1+b1
             
+            # define losses
+            #tf.losses.mean_squared_error(self.network, self.u)
+            #loss = tf.get_collection(tf.GraphKeys.LOSSES)
+            
+            # define cost
+            self.u_ = tf.nn.softmax(self.network)
+            cost = tf.reduce_mean(-tf.reduce_sum(self.u_ * tf.log(self.u), reduction_indices=[1]))
+            
+            # define training step
+            train_step = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(cost)
+            
+            # weights before training
+            print(self.weights[1].eval())
+            
+            # train
+            for i in range(1):
+                batch = nnm.formatBatch(25)
+                train_step.run(feed_dict={self.s: batch[0], self.u: batch[1]})
+                
+            # weights after training
+            print(self.weights[1].eval())
+            
+            sess.close()
+            
+        # get a batch for our linear function test setup
+        def linearFunctionBatch(self, size):
+            x = []
+            y = []
+            for i in range(size):
+                e_x = []
+                e_y = []
+                tmp = 0
+                for j in range(4):
+                    e_x.append(round(random.random()*10))
+                    tmp += e_x[j]*0.25
+                
+                tmp += 1
+                e_y.append(tmp)
+                x.append(e_x)
+                y.append(e_y)
+            return [x, y]
+            
+        # In order to test all the tensor flow functionality and get
+        # my head around the framework this will be a demonstration
+        # of a simple linear system that has to average some input and 
+        # add a bias with 4 random inputs thus: Y = 1/4*X + 1 = W*X + B
+        # we will setup tensorflow such that it will find the values of W and B
+        # by comparing its output to a function which already contains this function
+        # it will thus try to learn that functions functionality
+        def tensorFlowWithLinearSystem(self):
+            # session
+            sess = tf.Session()
+            
+            # tensor placeholders which will be loaded by training batches
+            x = tf.placeholder(tf.float32, shape=[None, 4])
+            y = tf.placeholder(tf.float32, shape=[None, 1])
+            
+            # weight and bias tensors 
+            # network has 4 inputs and only 2 layers (input, output) with 1 neuron
+            W = tf.Variable(tf.random_normal([4, 1]), tf.float32)
+            b = tf.Variable(tf.random_normal([1]), tf.float32)
+            
+            model = tf.add(tf.matmul(x,W), b)
+            
+            sess.run(tf.global_variables_initializer())
+            
+            squared_delta = tf.square(model - y)
+            loss = tf.reduce_sum(squared_delta)
+            
+            optimizer = tf.train.GradientDescentOptimizer(0.0005)
+            train = optimizer.minimize(loss)
+
+            # training
+            for i in range(1000):
+                #batch = self.linearFunctionBatch(1)
+                #sess.run(train, {x: batch[0], y: batch[1]})
+                sess.run(train, {x: [[1,2,3,4]], y: [[3.5]]})
+                if i%100 == 0:
+                    #print(sess.run(loss, {x: batch[0], y: [[3.5]]}))
+                    print(sess.run(loss, {x: [[1,2,3,4]], y: [[3.5]]}))
+                
+            print(sess.run([W, b]))
+            print(sess.run(model, {x : [[1,2,3,4]]}))
             
         # Training function
         def train(self):
--- NeuralNetworkManager.py
+++ NeuralNetworkManager.py
@@ -1,9 +1,11 @@
 # Class which will handle all the work done on neural networks and will contain all the functions which
 # are called in tensorflow in order to generate neural networks from the controllers.
 
+import tensorflow as tf
 from MLP2 import MLP
 from BinaryEncoderDecoder import BinaryEncoderDecoder
 from enum import Enum
+import random
 
 class NNTypes(Enum):
       MLP = 1
@@ -31,6 +33,8 @@ class NeuralNetworkManager:
         
         self.controller = None;
         
+        self.bed = BinaryEncoderDecoder()
+        
         self.num_input_neurons = 0
         self.num_output_neurons = 0
         
@@ -49,13 +53,11 @@ class NeuralNetworkManager:
     # Calculate the required amount of neural needed for both the input and the output based on the amount of 
     # binary numbers required to fully describe the state space and input space
     def calculateIONeurons(self):
-        bed = BinaryEncoderDecoder()
-        
         h_state = self.controller.getHighestStateID()
         h_input = self.controller.getHighestInputID()
         
-        b_state = bed.sntob(h_state)
-        b_input = bed.sntob(h_input)
+        b_state = self.bed.sntob(h_state)
+        b_input = self.bed.sntob(h_input)
         
         self.num_input_neurons = len(b_state)
         self.num_output_neurons = len(b_input)
@@ -77,6 +79,46 @@ class NeuralNetworkManager:
         layers[num_hidden_layers + 1] = self.num_output_neurons
         
         return layers
+    
+    # Format a single state into a binary encoded pair
+    def formatSingleState(self, id):     
+        pair = self.controller.getPairFromStateId(id)
+        
+        if(pair == None): return None
+        
+        pair[0] = self.bed.ntob(pair[0], self.num_input_neurons)
+        pair[1] = self.bed.ntob(pair[1], self.num_output_neurons)
+        
+        return pair
+    
+    # Format a training batch from controller data for NN training for binary encoding of controller indices into a tensor
+    def formatBatch(self, size):
+        pairs = []
+        
+        lowest_state = self.controller.getLowestStateID()
+        highest_state = self.controller.getHighestStateID()
+        
+        while len(pairs) < size:
+            s = round(random.random()*(highest_state - lowest_state) + lowest_state)
+            p = self.formatSingleState(s)
+            if(p != None):
+                pairs.append(p)
+                
+        x = []
+        y = []
+        for i in range(size):
+            p = pairs[i]
+            e_x = []
+            e_y = []
+            for j in range(self.num_input_neurons):
+                e_x.append(int(p[0][j]))
+            for j in range(self.num_output_neurons):
+                e_y.append(int(p[1][j]))
+            
+            x.append(e_x)
+            y.append(e_y)
+        
+        return [x, y]
             
     def initialize(self, type, training_method, controller):
         self.type = type
@@ -93,7 +135,8 @@ class NeuralNetworkManager:
         
         # initialize nn in itself
         # self.nn.initializeNetwork()
-        self.nn.tensorFlowTest()
+        # self.nn.tensorFlowTest(self)
+        self.nn.tensorFlowWithLinearSystem()
         
         # set training method
         
